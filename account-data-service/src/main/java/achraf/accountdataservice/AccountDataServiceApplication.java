47bb734de5d0f0b56399243264b91f3284637a82
achrafhammi <achrafhammi29@gmail.com>
2024-05-14 02:55:29 +0100

    TP6 CONNECTORS PART2 DONE

---
+++ account-data-service/src/main/java/achraf/accountdataservice/AccountDataServiceApplication.java
@@ -0,0 +1,13 @@
+package achraf.accountdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountDataServiceApplication.class, args);
+    }
+
+}
